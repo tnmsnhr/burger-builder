f56896866afc37c0aea1bbdfd122f54b55e8056a
Tanmoy Singha Roy <tnmsnhr@gmail.com>
2020-10-04 19:26:26 +0530

    Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import * as actions from './store/actions/index';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import asyncComponent from './hoc/asyncComponents/asyncComponent';
+
+
+const asyncCheckout = asyncComponent(()=>{
+  return import('./containers/Checkout/Checkout')
+});
+
+const asyncOrders = asyncComponent(()=>{
+  return import('./containers/Orders/Orders')
+});
+
+const asyncAuth = asyncComponent(()=>{
+  return import('./containers/Auth/Auth')
+});
 
 class App extends Component {
 
@@ -19,7 +30,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Redirect to ='/' />
       </Switch>
     )
@@ -27,9 +38,9 @@ class App extends Component {
     if(this.props.token){
       routes =(
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to ='/' />
